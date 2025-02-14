d4594657ea71b11cd7a2b919959ad2f802da326c
mohamed-a95 <mohamedabdihakima95@gmail.com>
2025-02-15 00:51:59 +0100

    reviderin

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -16,42 +16,33 @@ const Intro: React.FC = () => {
         Din webbläsare stöder inte videon.
       </video>
 
-      {/* Mörkt överlägg för bättre läsbarhet */}
       <div className="absolute inset-0 bg-black/65"></div>
 
-      {/* Innehållscontainer – justerad för att alltid vara synlig */}
-      <div className="relative flex flex-col justify-center sm:justify-start items-center h-full text-white text-center px-6">
-        {/* Extra padding-top för att inte överlappa headern */}
-        <div className="mt-20 sm:mt-35">
-          <h1 className="text-3xl sm:text-4xl font-bold uppercase">
-            Välkommen till <br />
-            Mamcon Stockholm AB
-          </h1>
+      <div className="relative flex flex-col justify-center items-center h-full text-white text-center px-6 sm:pt-20">
+        <h1 className="text-4xl font-bold uppercase">
+          Välkommen till <br />
+          Mamcon Stockholm AB
+        </h1>
 
-          {/* Gul linje – synlig på alla skärmar */}
-          <div className="bg-yellow-500 h-[3px] w-[80%] sm:w-[500px] my-4 mx-auto"></div>
+        <div className="bg-yellow-500 h-[3px] w-[500px] my-4"></div>
 
-          <p className="mt-6 px-5 max-w-[700px] text-lg sm:text-xl font-normal">
-            Vi är din expertpartner inom bygg- och projektledning,
-            specialiserade på mark- och anläggningsarbeten, infrastruktur och
-            stensättning. Med bred erfarenhet som byggledare, projektledare,
-            entreprenadingenjör och arbetsledare ser vi till att ditt projekt
-            genomförs effektivt, kostnadssmart och med högsta kvalitet – från
-            planering till färdigställande.
-          </p>
+        <p className="mt-6 px-5 max-w-[700px] text-xl text-center font-normal">
+          Vi är din expertpartner inom bygg- och projektledning, specialiserade
+          på mark- och anläggningsarbeten, infrastruktur och stensättning. Med
+          bred erfarenhet som byggledare, projektledare, entreprenadingenjör och
+          arbetsledare ser vi till att ditt projekt genomförs effektivt,
+          kostnadssmart och med högsta kvalitet – från planering till
+          färdigställande.
+        </p>
 
-          {/* Knappen – alltid synlig */}
-          <div className="flex justify-center mt-10 sm:mt-14 gap-6">
-            <ScrollLink to="services" smooth={true} duration={500}>
-              <button className="bg-yellow-500 text-black py-3 px-6 font-semibold uppercase hover:bg-yellow-600 rounded-md sm:pb-10">
-                Våra tjänster
-              </button>
-            </ScrollLink>
-          </div>
+        <div className="flex justify-center mt-14 gap-6">
+          <ScrollLink to="services" smooth={true} duration={500}>
+            <button className="bg-yellow-500 text-black py-3 px-6 font-semibold uppercase hover:bg-yellow-600 rounded-md">
+              våra tjänster
+            </button>
+          </ScrollLink>
         </div>
       </div>
     </div>
   );
 };
-
-export default Intro;
