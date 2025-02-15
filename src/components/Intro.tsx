01791dcf0dcda4461dd4f28d9c60884343bd917c
mohamed-a95 <mohamedabdihakima95@gmail.com>
2025-02-15 01:38:59 +0100

    åter

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -18,22 +18,15 @@ const Intro: React.FC = () => {
 
       <div className="absolute inset-0 bg-black/65"></div>
 
-      {/* Anpassning endast för skärmar under 690px */}
-      <div
-        className="relative flex flex-col justify-center items-center h-full text-white text-center px-6 
-                      pt-[15rem] max-[689px]:pt-[12rem]"
-      >
-        {/* Rubrik – Fixat för skärmar mindre än 690px */}
-        <h1 className="text-4xl font-bold uppercase max-[689px]:text-3xl max-[689px]:leading-snug">
+      <div className="relative flex flex-col justify-center items-center h-full text-white text-center px-6">
+        <h1 className="text-4xl font-bold uppercase">
           Välkommen till <br />
           Mamcon Stockholm AB
         </h1>
 
-        {/* Gul linje – Anpassad för mindre skärmar */}
-        <div className="bg-yellow-500 h-[3px] w-[500px] my-4 max-[689px]:w-[80%]"></div>
+        <div className="bg-yellow-500 h-[3px] w-[500px] my-4"></div>
 
-        {/* Text – Sänkt margin på små skärmar */}
-        <p className="mt-6 px-5 max-w-[700px] text-xl text-center font-normal max-[689px]:mt-4 max-[689px]:text-lg">
+        <p className="mt-6 px-5 max-w-[700px] text-xl text-center font-normal">
           Vi är din expertpartner inom bygg- och projektledning, specialiserade
           på mark- och anläggningsarbeten, infrastruktur och stensättning. Med
           bred erfarenhet som byggledare, projektledare, entreprenadingenjör och
@@ -42,14 +35,10 @@ const Intro: React.FC = () => {
           färdigställande.
         </p>
 
-        {/* Knapp – Gör den tydlig på små skärmar */}
-        <div className="flex justify-center mt-14 gap-6 max-[689px]:mt-8">
+        <div className="flex justify-center mt-14 gap-6">
           <ScrollLink to="services" smooth={true} duration={500}>
-            <button
-              className="bg-yellow-500 text-black py-3 px-6 font-semibold uppercase 
-                               hover:bg-yellow-600 rounded-md max-[689px]:py-4 max-[689px]:px-8 max-[689px]:text-lg"
-            >
-              Våra tjänster
+            <button className="bg-yellow-500 text-black py-3 px-6 font-semibold uppercase hover:bg-yellow-600 rounded-md">
+              våra tjänster
             </button>
           </ScrollLink>
         </div>
