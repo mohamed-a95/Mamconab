df24434fdec24fcdc358313ef238a207af9cf6fe
mohamed-a95 <mohamedabdihakima95@gmail.com>
2025-02-15 01:46:20 +0100

    ytterligare ändring

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -18,13 +18,15 @@ const Intro: React.FC = () => {
 
       <div className="absolute inset-0 bg-black/65"></div>
 
-      <div className="relative flex flex-col justify-center items-center h-full text-white text-center px-6">
-        <h1 className="text-4xl font-bold uppercase">
+      {/* Standard: ingen extra padding, men på skärmar mindre än 560px läggs pt-20 */}
+      <div className="relative flex flex-col justify-center items-center h-full text-white text-center px-6 pt-0 max-[560px]:pt-20">
+        <h1 className="text-4xl max-[560px]:text-3xl font-bold uppercase">
           Välkommen till <br />
           Mamcon Stockholm AB
         </h1>
 
-        <div className="bg-yellow-500 h-[3px] w-[500px] my-4"></div>
+        {/* Gul linje: fast bredd upp till 500px, men på små skärmar tar den 90% av bredden */}
+        <div className="bg-yellow-500 h-[3px] w-full max-w-[500px] max-[560px]:w-[90%] my-4"></div>
 
         <p className="mt-6 px-5 max-w-[700px] text-xl text-center font-normal">
           Vi är din expertpartner inom bygg- och projektledning, specialiserade
@@ -37,7 +39,7 @@ const Intro: React.FC = () => {
 
         <div className="flex justify-center mt-14 gap-6">
           <ScrollLink to="services" smooth={true} duration={500}>
-            <button className="bg-yellow-500 text-black py-3 px-6 font-semibold uppercase hover:bg-yellow-600 rounded-md">
+            <button className="bg-yellow-500 text-black py-3 px-6 max-[560px]:py-2 max-[560px]:px-4 font-semibold uppercase hover:bg-yellow-600 rounded-md">
               våra tjänster
             </button>
           </ScrollLink>
